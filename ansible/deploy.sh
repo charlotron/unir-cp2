ede1596a8f1342b8b9c9837cb78635bee5ddaa00
Carlos Cenjor Ramirez <carlos.cenjor@sngular.com>
2022-03-04 20:13:38 +0100

    nfs working

--- ansible/deploy.sh
+++ ansible/deploy.sh
@@ -6,3 +6,4 @@ ansible-playbook -i hosts 10-nfs-dir-share.yml -v --ssh-common-args='-o StrictHo
 ansible-playbook -i hosts 20-install-k8s.yml -v --ssh-common-args='-o StrictHostKeyChecking=no'
 ansible-playbook -i hosts 21-configure-k8s-master.yml -v --ssh-common-args='-o StrictHostKeyChecking=no'
 ansible-playbook -i hosts 22-configure-k8s-workers.yml -v --ssh-common-args='-o StrictHostKeyChecking=no'
+ansible-playbook -i hosts 30-deploy-k8s-pods.yml -v --ssh-common-args='-o StrictHostKeyChecking=no'
